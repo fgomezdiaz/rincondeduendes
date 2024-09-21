1a7b03ae34011394bf1d1ad1e80d4798881cffd0
fgomezdiaz <pacoymari@gmail.com>
2024-09-21 17:37:34 +0200

    Ajustes varios

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,15 @@ import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
 
 
+//import node from '@astrojs/node';
+
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'static',
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
   adapter: vercel(),
 });
